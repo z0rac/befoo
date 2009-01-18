5063954f938028c4627830a1b18c3b4e841c714f
z0rac <z0rac@e8355a02-bfb8-4361-ab08-856b54ffdbd8>
2009-01-18 02:27:10 +0000

    Support anoonymous login. Fix setting example.

--- src/define.h
+++ src/define.h
@@ -9,7 +9,7 @@
 #define WINVER _WIN32_WINNT
 
 #define APP_NAME               "befoo"
-#define APP_VERSION            "0.9"
+#define APP_VERSION            "0.9.1"
 
 #define ID_MENU_FETCH          1
 #define ID_MENU_SUMMARY        2
